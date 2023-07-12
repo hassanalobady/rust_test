6dbf4a1128c3829d7151948fcd3688ea9428b419
hassanalobady <hassanalobady@gmail.com>
2023-07-13 03:55:06 +0800

    commit

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ extern crate sha2;
 //extern crate chacha20;
 //extern crate rug;
 
-use sha2::Sha512;
-use rand::RngCore;
+
+
 //use std::convert::TryInto;
 //use bls12_381::{G1Projective, Scalar};
 use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar as RistrettoScalar};
@@ -18,13 +18,13 @@ use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar as RistrettoSca
 //use chacha20::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
 //use hex_literal::hex;
 //use std::env;
-use curve25519_dalek::constants;
+
 use curve25519_dalek::scalar::Scalar;
 // use rand::prelude::*;
-use sha2::{Sha256, Digest};
-use rand::thread_rng;
+
+
 use rand_core::OsRng;
-use std::env;
+
 
 // Schnorr Signatures
 //1. Generates a random private key, generates the corresponding public key, 
@@ -34,7 +34,7 @@ use std::env;
 // key genration of the Schnorr Signatures
 pub fn schnorr_keygen() -> (Scalar, RistrettoPoint)
  {
-    let mut rng = rand::thread_rng();
+    let _rng = rand::thread_rng();
     let private_key = Scalar::random(&mut OsRng);
     let public_key = private_key * RistrettoPoint::default();
     (private_key, public_key)
@@ -42,12 +42,12 @@ pub fn schnorr_keygen() -> (Scalar, RistrettoPoint)
 
 // signs random message of the Schnorr Signatures
 
-pub fn schnorr_sign(private_key: Scalar, message: &[u8]) -> (RistrettoPoint, Scalar) 
+pub fn schnorr_sign(private_key: Scalar, _message: &[u8]) -> (RistrettoPoint, Scalar) 
 
 {
     // Challenge generation
-   let mut temp: [u8;32] = [0u8;32];
-    let mut os_rng = rand::thread_rng();
+   let temp: [u8;32] = [0u8;32];
+    let _os_rng = rand::thread_rng();
     let nonce = RistrettoScalar::random(&mut OsRng);
     let r = nonce * RistrettoPoint::default();
     let e = Scalar::from_bytes_mod_order(temp);
@@ -57,10 +57,10 @@ pub fn schnorr_sign(private_key: Scalar, message: &[u8]) -> (RistrettoPoint, Sca
 
 // the verifies of random message Schnorr signature.
 
-pub fn schnorr_verify(public_key: RistrettoPoint, signature: (RistrettoPoint, Scalar), message: &[u8]) -> bool 
+pub fn schnorr_verify(public_key: RistrettoPoint, signature: (RistrettoPoint, Scalar), _message: &[u8]) -> bool 
 
 {
-    let mut temp: [u8;32] = [0u8;32];
+    let temp: [u8;32] = [0u8;32];
     let (r, s) = signature;
     let e = Scalar::from_bytes_mod_order(temp);
     let rv = s * RistrettoPoint::default() - e * public_key;
